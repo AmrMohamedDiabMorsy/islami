b9b663543f072f1afd1259fe63df81df5b265352
01554859516 <01554859516@gmail.com>
2023-09-20 18:54:22 +0300

    load hadeth file

--- lib/tabs/ahadeth.dart
+++ lib/tabs/ahadeth.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/ahadeth_details.dart';
+import 'package:islami/my_theme_data.dart';
 
-class ahadethTab extends StatelessWidget {
-  const ahadethTab({super.key});
+import 'hadeth_model.dart';
+
+class ahadethTab extends StatefulWidget {
+
+  @override
+  State<ahadethTab> createState() => _ahadethTabState();
+}
+
+class _ahadethTabState extends State<ahadethTab> {
+ List<Hadethmodel>allhadeth = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple,
+    if (allhadeth.isEmpty) {
+      loadHadeth();
+    }
+
+    return Column(
+      children: [
+        Center(child: Image.asset("assets/images/ahadeth_image.png")),
+        Divider(
+          thickness: 3,
+          color: MyThemeData.primary,
+        ),
+        Text("Ahadeth",),
+        Divider(
+          thickness: 3,
+          color: MyThemeData.primary,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.separated(
+              separatorBuilder:(context , index)=> Divider(
+                thickness: 1,
+                endIndent: 40,
+                indent: 40,
+                color: MyThemeData.primary,
+              ),
+    itemBuilder:(context , index){
+      return InkWell(
+               onTap: (){
+                 Navigator.pushNamed(context, AhadethDedails.routeName,
+                  arguments:  allhadeth[index]
+                 );
+               },
+
+            child: Text(allhadeth[index].title, textAlign: TextAlign.center,));
+    },
+              itemCount: allhadeth.length,
+    ),
+          ),
+        )
+
+      ],
     );
+  }
+
+  loadHadeth()async{
+    rootBundle.loadString("assets/files/ahadeth.txt").
+   then((ahadeth){
+     List<String>ahadethList= ahadeth.split("#");
+     for(int i = 0  ; i <ahadethList.length ; i++){
+       String  hadethone = ahadethList[i];
+       List<String>hadethonelines =hadethone. trim().split("\n");
+       String title = hadethonelines[0];
+       hadethonelines.removeAt(0);
+       List<String>content=  hadethonelines;
+       Hadethmodel  hadethmodel =  Hadethmodel(title ,content);
+       allhadeth.add(hadethmodel);
+       print(i);
+       print(title);
+     }
+     setState(() {
+
+     });
+   }).catchError((e){
+     print(e.toString());
+   });
   }
 }
